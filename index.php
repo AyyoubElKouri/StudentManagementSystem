2315c1ab817970f3b8ffc1944a1648aaca21d501
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-04-25 17:13:08 +0100

    feat(NavBar): implement navigation bar

--- index.php
+++ index.php
@@ -4,13 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Management System</title>
+    <link rel="stylesheet" href="style.css">
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
+        crossorigin="anonymous"
+    >
 </head>
 <body>
-    <h1>This is a SMS, (Student Management System)</h1>
-    <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, facere, labore ex <br>
-        repellendus voluptate harum et eveniet voluptatum animi odit tempora, consectetur  <br>
-        delectus eaque dignissimos aliquid autem ullam aut enim.
-    </p>
+    <!-- Start Navbar -->
+    <nav>
+        <label class="logo">W-School</label>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="" class="btn btn-success">Login</a></li>
+        </ul>
+    </nav>
+    <!-- End Navbar -->
+
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
+        crossorigin="anonymous"
+    ></script>
+
 </body>
 </html>
