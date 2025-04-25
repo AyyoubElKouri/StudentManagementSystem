35db5efffef3052f2dc8f05f68c94413d0220ed5
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-04-25 17:23:31 +0100

    feat(section1): implement the first section

--- index.php
+++ index.php
@@ -24,6 +24,14 @@
     </nav>
     <!-- End Navbar -->
 
+    <!-- Start section 1 -->
+    <div class="section1">
+        <label class="img-text">We Teach Students With Car</label>
+        <img class="main-img" src="assets/school.png" alt="Error">
+    </div>
+    <!-- End section 1 -->
+
+
     <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
