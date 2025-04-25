5af48411b0cb511ec9b60b30cd8e857c06a82822
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-04-25 16:43:57 +0100

    chore(project): initial project structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+</head>
+<body>
+    <h1>This is a SMS, (Student Management System)</h1>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, facere, labore ex <br>
+        repellendus voluptate harum et eveniet voluptatum animi odit tempora, consectetur  <br>
+        delectus eaque dignissimos aliquid autem ullam aut enim.
+    </p>
+</body>
+</html>
